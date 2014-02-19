5c83fda969e3ec9ee87e292c0f3479f21b10f482
Matthew J.W. Basterfield <mjwb@donateafrica.org>
2014-02-19 17:57:24 +0200

    Routed static_pages#home to application root

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static Pages" do
   describe "Home Page" do
     it "should have the content 'StaticPages#home'" do
-      visit 'static_pages/home'
+      visit '/'
       expect(page).to have_content('StaticPages#home')
     end
   end
