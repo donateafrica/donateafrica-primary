c59d9ff421858a7a10eba046d7ccef5d404c3322
Matthew J.W. Basterfield <mjwb@donateafrica.org>
2014-02-19 05:36:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DonateafricaPrimary::Application.config.secret_key_base = 'cf9527c322858166aec7882072981267c8af43a00fc0ae1aa2942cb243c283480c08d658bc912d9e43f837b7b5b8c569ba1fa0a7af22f5ee2f7292e07722b337'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DonateafricaPrimary::Application.config.session_store :cookie_store, key: '_donateafrica-primary_session'
