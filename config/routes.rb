e1d0babc73948133852d436d91dc06f96e5a133d
Matthew J.W. Basterfield <mjwb@donateafrica.org>
2014-02-26 00:10:20 +0200

    Create users resource RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 DonateafricaPrimary::Application.routes.draw do
-  get "users/new"
  root 'static_pages#home'
+ resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
