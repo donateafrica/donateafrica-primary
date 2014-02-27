cf75e1cc161727acb3ebd715adea27b99d1a8082
Matthew J.W. Basterfield <mjwb@donateafrica.org>
2014-02-27 15:29:23 +0200

    Added user signup form and form validation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,9 +12,36 @@ describe "User Pages" do
   end
 
   describe "Register Page" do
-    it "should have the content 'Users#new'" do
-      visit '/users/new'
-      expect(page).to have_content('Users#new')
+    it "should have the content 'Register'" do
+      visit new_user_path
+      expect(page).to have_content('Register')
+    end
+  end
+
+  describe "registration" do
+
+    before { visit new_user_path }
+
+    let(:submit) { "Register" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "First name",   with: "First Name"
+        fill_in "Last name",	with: "Last Name"
+        fill_in "Email",        with: "first.last@example.com"
+        fill_in "Password",     with: "test123"
+        fill_in "Confirmation", with: "test123"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
     end
   end
 end
